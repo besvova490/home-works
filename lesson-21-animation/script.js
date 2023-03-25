3d9e5fbfd33fa1f445a2c0460e926f661b03fa96
volodymyr-coax-dev <volodymyr.sen@coaxsoft.com>
2023-03-25 14:21:32 +0200

    main addedforms and animaitons

---
+++ lesson-21-animation/script.js
@@ -0,0 +1,10 @@
+const arrowFunc = () => console.log("Hello World1");
+
+function functionDeclaration() {
+  console.log("Hello World1");
+}
+
+(function() {
+
+  console.log("Hello World1");
+} ());
