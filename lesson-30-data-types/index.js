0672cb5292060768e67f10a0c666574d09472969
volodymyr-coax-dev <volodymyr.sen@coaxsoft.com>
2023-04-24 21:21:20 +0300

    added lesson 30

---
+++ lesson-30-data-types/index.js
@@ -0,0 +1,85 @@
+// NUMBERS
+
+// const number = 1;
+// const numberBig = 112313121231231232131231123123;
+// const string = "A";
+// const boolean = true;
+// const sym = Symbol("foo");
+// const UndefinedType = undefined;
+// const nullType = null;
+
+// const array = [];
+// const object = {};
+
+// const someNumber = 0;
+// const someOtherNumber = 2.10;
+
+// // const sum = someNumber * someOtherNUmber;
+// console.log();
+
+// STRINGS
+
+
+// const hello = "Hello";
+// const world = "World";
+
+// console.log(hello + world + "!");
+// console.log(`Hello ${1} ${hello} Hello ${world}! some text Hello Hello Hello`.replace("Hello", "*"));
+// console.log(`Hello ${1} ${hello} Hello ${world}! some text Hello Hello Hello`[0]);
+
+// const email = "      example@gamil.com         ";
+// console.log(email.trim())
+
+
+// const stopSignal = "StOp";
+// console.log(stopSignal);
+// console.log(stopSignal.toLowerCase());
+// console.log(stopSignal.toUpperCase());
+
+
+// const greeting = "Hello World!";
+// console.log(greeting.length);
+
+// BOOLEANS
+
+// const name = "Joh";
+
+// console.log(`Hello ${name || "Default Name"}`);
+
+
+// const age = 72;
+
+// if (age > 7 || age < 70) {
+//   console.log("Hello");
+// }
+
+// if (age > 7 && age < 70) {
+//   console.log("Hello");
+// }
+
+const newArray = ["Message 1", "Message 2", "......", "Message 100"];
+// console.log(newArray[0]);
+// newArray[2] = "Message n + 1"
+// newArray[newArray.length] = `Message ${newArray.length}`;
+// newArray.push(`Message ${newArray.length}`);
+// newArray.pop();
+
+// newArray.unshift("Message 0");
+// newArray.shift();
+// newArray.splice(2)
+// console.log(newArray);
+
+// console.log(newArray.slice(2));
+// console.log(newArray);
+
+// let variable = null;
+// console.log(variable);
+
+// variable = newArray.slice(2);
+// console.log(variable, newArray);
+
+let number = 1;
+// number = number + 1;
+// number += 1;
+// number++
+console.log(number);
