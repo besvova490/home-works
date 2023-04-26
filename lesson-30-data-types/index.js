9dcb6a6f59422e12f76544d9e4b956e5980d820e
volodymyr-coax-dev <volodymyr.sen@coaxsoft.com>
2023-04-26 21:19:27 +0300

    main

--- lesson-30-data-types/index.js
+++ lesson-30-data-types/index.js
@@ -1,3 +1,5 @@
+// import { faker } from '@faker-js/faker';
+// const { faker } = require('@faker-js/faker')
 // NUMBERS
 
 // const number = 1;
@@ -18,8 +20,6 @@
 // console.log();
 
 // STRINGS
-
-
 // const hello = "Hello";
 // const world = "World";
 
@@ -41,7 +41,6 @@
 // console.log(greeting.length);
 
 // BOOLEANS
-
 // const name = "Joh";
 
 // console.log(`Hello ${name || "Default Name"}`);
@@ -57,6 +56,7 @@
 //   console.log("Hello");
 // }
 
+// ARRAYS
 const newArray = ["Message 1", "Message 2", "......", "Message 100"];
 // console.log(newArray[0]);
 // newArray[2] = "Message n + 1"
@@ -78,8 +78,157 @@ const newArray = ["Message 1", "Message 2", "......", "Message 100"];
 // variable = newArray.slice(2);
 // console.log(variable, newArray);
 
-let number = 1;
+// let number = 1;
 // number = number + 1;
 // number += 1;
 // number++
-console.log(number);
+// console.log(number);
+
+// let newIndex = 0;
+// for (let index = 0; index < 10; ++index) {
+//   console.log(index);
+//   // console.log(newIndex++);
+// }
+// console.log("loop end")
+
+// console.log(newIndex);
+
+//OBJECTS
+// const person = {
+//   name: "Joh",
+//   secondName: false,
+//   surname: "Smit",
+//   age: 25,
+//   email: "example@gami.com",
+//   isOpenToWork: true,
+//   lastWorkDate: new Date(),
+//   carLogo: null,
+//   closes: ["1", "2", "3"],
+//   false: true,
+// };
+
+// const users = [
+//   {},
+//   {},
+//   {},
+//   {},
+// ];
+
+// console.log([3, 4, 1, 0]);
+
+// const usersList = new Array(5).fill(null).map(() => ({
+//   name: faker.name.firstName(),
+//   surname: faker.name.lastName(),
+//   age: faker.datatype.number(100),
+//   email: faker.internet.email(),
+//   isOpenToWork: faker.datatype.boolean(),
+// }))
+
+const usersList = [
+  {
+    name: 'Sierra',
+    surname: 'Quigley',
+    age: 14,
+    email: 'Alfreda12@yahoo.com',
+    isOpenToWork: false
+  },
+  {
+    name: 'Barton',
+    surname: 'Corkery',
+    age: 100,
+    email: 'Dewitt.Kohler71@gmail.com',
+    isOpenToWork: true
+  },
+  {
+    name: 'Demetrius',
+    surname: 'Mohr',
+    age: 90,
+    email: 'Maureen55@hotmail.com',
+    isOpenToWork: false
+  },
+  {
+    name: 'Abby',
+    surname: 'Metz',
+    age: 26,
+    email: 'Zelda93@yahoo.com',
+    isOpenToWork: false
+  },
+  {
+    name: 'Connie',
+    surname: 'Legros',
+    age: 86,
+    email: 'Gerardo91@yahoo.com',
+    isOpenToWork: true
+  }
+];
+
+// User "name" "surname", user age is "age", "email" 
+// User Sierra Quigley, user age is 14, Alfreda12@yahoo.com 
+
+// OBJECTS ITERATION (LOOP)
+// console.log('User "name" "surname", user age is "age", "email" ');
+// usersList.forEach(item => {
+//   const { name, surname, age, email } = item;
+//   console.log(`user name is ${userName}`);
+// });
+
+// HOW TO GET LAST ELEMENT FROM ARRAY
+// const lastName = usersList[usersList.length - 1];
+// const lastName = usersList.slice(-1);
+// const lastName = usersList.splice(-1);
+// const lastName = usersList.pop();
+// console.log(lastName, usersList.length);
+
+// display user data
+// console.log(`Hey my name is ${person["lastWorkDate"]} and my email is ${person.email}`);
+
+
+const person = {
+  name: 'Connie',
+  surname: 'Legros',
+  age: 86,
+  email: 'Gerardo91@yahoo.com',
+  isOpenToWork: true,
+  car: {
+    color: "red",
+    name: "BMW"
+  },
+};
+
+// const firstName = person.name;
+// const surName = person.surname;
+// const age = person.age;
+
+// const car = { color: "red" };
+
+// rest operator js destructuring
+// console.log(car.color);
+// const { color: carColor } = car;
+// const carColor = car.color;
+// console.log(carColor, car);
+
+// rest operator js destructuring
+const { name, surname, ...otherUserData } = person;
+// const otherUserData = {
+//   age: person.age,
+//   email: person.email,
+//   isOpenToWork: person.isOpenToWork,
+//   car: person.car,
+// };
+
+// rest operator js destructuring
+// const { name: carName, color } = car;
+// // const carName = car.name;
+// console.log(carName, color);
+// console.log(name, surname, otherUserData);
+
+const fruits = ["apple", "banana", "blueberry", "orange"];
+// const apple = fruits[0];
+// const banana = fruits[1];
+// const blueberry = fruits[2];
+// const orange = fruits[3];
+// const [orange, ...otherFruits] = fruits;
+const [...otherArray] = fruits;
+
+console.log(otherArray, fruits);
+
